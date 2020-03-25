f531c11b055a9406c43599d6eed139238cce0ea3
lentapple3922 <56277764+lentapple3922@users.noreply.github.com>
2020-03-25 16:41:30 -0400

    Playing with Flow Controols

---
+++ ATBSWP/Mu-Files/Flow Control Statments.py
@@ -0,0 +1,9 @@
+# Write your code here :-)
+name = input()
+password = input()
+if name =='Mary':
+    print('Hello, Mary')
+    if password =='swordfish':
+        print('Access granted.')
+    else:
+        print('Wrong password')
