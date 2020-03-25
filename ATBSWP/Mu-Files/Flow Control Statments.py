82e5783a1ce390c3163b61fd34f9fe0f5790cce8
lentapple3922 <56277764+lentapple3922@users.noreply.github.com>
2020-03-25 17:04:58 -0400

    Added Who are you

--- ATBSWP/Mu-Files/Flow Control Statments.py
+++ ATBSWP/Mu-Files/Flow Control Statments.py
@@ -1,11 +1,13 @@
-# Write your code here :-)
+# Basic Username and Password Prompt
 print('What is your name?')
-name = input()
+name = input()                  #Asks for Username
 print('What is your Password')
-password = input()
+password = input()              #Asks for password
 if name =='Mary':
-    print('Hello, Mary')
-    if password =='swordfish':
+    print('Hello, '+ name)
+    if password =='swordfish':  #Checks if Password is right
         print('Access granted.')
     else:
         print('Wrong password')
+else:
+    print('who are you')
