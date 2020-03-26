dde1a723b90b92fd0405f109a622ab620e22405b
lentapple3922 <56277764+lentapple3922@users.noreply.github.com>
2020-03-25 20:00:31 -0400

    Program I am using to learn how to use if, elif, else

---
+++ ATBSWP/Make an Account with Us.py
@@ -0,0 +1,21 @@
+#This program is to let a user log in or create an account. Can only hold two people, new user and old user
+print('Do you have an account?')
+account = input()
+if(account=='yes'):
+    print('What is your username?')
+    accountName = input()
+    if(accountName == Kailan):
+        print('What is your password')
+        accountPassword = input()
+        if(accountPassword == ksh1234):
+            print('welcome'+ accountName)
+        else:
+            print('Wrong Password')
+    else:
+        print('User does not exsist')
+else:
+    print('Enter new username')
+    newUser = input()
+    print('Enter password')
+    newPassword = input()
+    print('Welcome, ' + newUser)
