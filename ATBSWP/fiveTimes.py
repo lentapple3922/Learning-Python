20c435b306461a2c52772e3fcb9c7bb602c6b690
lentapple3922 <56277764+lentapple3922@users.noreply.github.com>
2020-03-28 16:47:50 -0400

    How to use a for and while loop

---
+++ ATBSWP/fiveTimes.py
@@ -0,0 +1,11 @@
+#uses for statment as loop
+print('My name is')
+for i in range(5):
+    print('Jimmy Five Times('+str(i)+')')
+
+#How to use while loop as a for loop
+print('My name is')
+i = 0
+while i<5:
+    print('Jimmy Five Times('+str(i)+')')
+    i = i + 1
