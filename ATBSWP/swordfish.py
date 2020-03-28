d1fbeec8a1969ff28107d1d7fea0158588a2f2a4
lentapple3922 <56277764+lentapple3922@users.noreply.github.com>
2020-03-27 20:44:07 -0400

    Update to last program

--- ATBSWP/swordfish.py
+++ ATBSWP/swordfish.py
@@ -1,6 +1,6 @@
-while (true):
+while (True):
     print('Who are you?')
-    name = imput()
+    name = input()
     if name != 'Joe':
         continue
     print('Hello, Joe. What is the password?(It is a fish.)')
