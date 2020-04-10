c707037cf82738e30ab0d1fb62bd7be69680e644
lentapple3922 <56277764+lentapple3922@users.noreply.github.com>
2020-04-10 12:19:08 -0400

    This is a program that runs the Collatz Sequence

--- ATBSWP/Chapter3Functions/theCollatzSequence.py
+++ ATBSWP/Chapter3Functions/theCollatzSequence.py
@@ -7,8 +7,20 @@ def collatz(number):
         intNumber = 3 * intNumber + 1
         return intNumber
 
-print('Type a number: ', end='')  # Asks for user input to run in collatz
-userInput = int(input())
-while userInput > 1:
-    print(collatz(userInput))
-    userInput = collatz(userInput)
+
+while True:  # Main game loop
+    try:  # to check if the user inputs a string
+        print('Type a number: ', end='')  # Asks for userInput to run in collatz
+        userInput = int(input())
+
+        while userInput > 1:  # Runs collatz function to get 1
+            print(collatz(userInput))
+            userInput = collatz(userInput)
+
+    except ValueError:  # Deals with user typing a string
+        print('Thats not a number, do you want to exit?')  # Asks user if they want to exit
+        endProgram = input()
+        if endProgram == 'Yes' or endProgram == 'yes':
+            break  # will end program
+        elif endProgram == 'No' or endProgram == 'no':
+            continue  # will continue the loop
